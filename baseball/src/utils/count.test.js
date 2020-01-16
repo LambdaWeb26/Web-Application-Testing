b5fe55bfa152da44d2d8ebd8037f3d87874c4aa8
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-01-16 15:55:44 -0700

    javascript test

--- baseball/src/utils/count.test.js
+++ baseball/src/utils/count.test.js
@@ -2,16 +2,20 @@ import '@testing-library/jest-dom/extend-expect';
 import {ballClick, strikeClick, foulClick} from "./count";
 
 test('strike button adds 1 to the strike count', () => {
-    expected(strikeClick(1).toEqual(2));
-})
+    let actual = strikeClick(1);
+    let expected = 2
+    
+    expect(actual).toBe(expected);
+    expect(strikeClick(1)).toBe(2);
+}),
 
 
 test('balls can only go up to 4 before reset', () => {
 
-    expected(ballClick(1).toEqual(2));
-})
+    expect(ballClick(1)).toEqual(2);
+}),
 
 test('you recieve only 2 fouls', () => {
     
-    expected(foulClick(1).toBe(2));
+    expect(foulClick(1)).toBe(2);
 })
